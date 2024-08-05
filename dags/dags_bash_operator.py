5e6a83217546e4ec28d5eaa5f4440bb72b4a3844
yoonju1030 <allrealjgt@gmail.com>
2024-08-05 22:20:52 +0900

    add empty directory

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -13,7 +13,7 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_operator",
     schedule="0 0 * * *",
-    start_date=pendulum.datetime(2024, 8, 5, 22, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 8, 5, 23, tz="Asia/Seoul"),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
     tags=["air", "flow"],
