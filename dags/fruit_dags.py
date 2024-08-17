e93889a5cc83abae5d586697480bbfce0e8361ef
yoonju1030 <allrealjgt@gmail.com>
2024-08-17 18:32:18 +0900

    change dag name

--- dags/fruit_dags.py
+++ dags/fruit_dags.py
@@ -4,7 +4,7 @@ from airflow.models.dag import DAG
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_bash_operator",
+    dag_id="fruit_bash_operator",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2024, 8, 5, 23, tz="Asia/Seoul"),
     catchup=False,
