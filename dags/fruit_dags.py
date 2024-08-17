aafee49478715de1beea7ee98333bb057e2b22f5
yoonju1030 <allrealjgt@gmail.com>
2024-08-17 18:30:40 +0900

    add dag for shell script

---
+++ dags/fruit_dags.py
@@ -0,0 +1,26 @@
+import datetime
+import pendulum
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2024, 8, 5, 23, tz="Asia/Seoul"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["fruit", "test"],
+) as dag:
+
+    t1_orange = BashOperator(
+        task_id="t1_orange",
+        bash_command="/home/allrealjgt/airflow_data/plugins/select_fruit.sh ORANGE"
+    )
+
+    t2_avocado = BashOperator(
+        task_id="t2_avocado",
+        bash_command="/home/allrealjgt/airflow_data/plugins/select_fruit.sh AVOCADO"
+    )
+
+    # task 수행 순서
+    t1_orange >> t2_avocado
