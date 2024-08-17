138a7b88520df698c145e34653767a943478e712
yoonju1030 <allrealjgt@gmail.com>
2024-08-18 01:13:55 +0900

    add dag for using task decorator

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,6 +1,11 @@
+from airflow.decorators import task
 import random
 
 def select_shohoku():
     fruit = ["akagi", "miyagi", "sakuragi", "rukawa", "mitsui"]
     rand_int = random.randint(0, len(fruit)-1)
     print(fruit[rand_int])
+
+@task(task_id="python_task_01")
+def print_context(some_input):
+    print(some_input)
