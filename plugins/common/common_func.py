3c0842ace9657c46727a85583a8dc424f5f8f9b5
yoonju1030 <allrealjgt@gmail.com>
2024-08-25 17:02:20 +0900

    Modify method to get anime info

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,6 +1,8 @@
 from airflow.decorators import task
 import random
-import laftel
+import requests
+import os
+import json
 
 def select_shohoku():
     fruit = ["akagi", "miyagi", "sakuragi", "rukawa", "mitsui"]
@@ -13,4 +15,11 @@ def print_context(some_input):
 
 @task(task_id="laftel_test")
 def get_info_anime():
-    laftel.sync.getAnimeInfo(query="슬램덩크")
+    url = os.environ["LAFTEL_URL"].format(query="슬램덩크")
+    headers={
+        "laftel": os.environ["LAFTEL_VALUE"],
+        "User-Agent": os.environ["USER_AGENT"]
+    }
+    resp_raw = requests.get(url=url, headers=headers)
+    resp = json.loads(resp_raw.text)
+    print(resp['results'][0]["img"])
